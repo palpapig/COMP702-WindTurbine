a0892b47a9a5cd30db30437a9c7b9301d740e79f
baileythorp04 <baileythorp04@gmail.com>
2026-05-07 11:37:33 +1200

    removed all Accord.NET code. Add comment guideline using Python and ONNX for training

--- COMP702-WindTurbine/services/DbService.cs
+++ COMP702-WindTurbine/services/DbService.cs
@@ -23,6 +23,7 @@ public sealed class DbService (
     public async Task<Turbine> GetTurbineById(string turbineId)
     {
         return await db.Turbine
+            .Include(t => t.DegradationModelDetails)
             .Include(t => t.TurbineModel)
                 .ThenInclude(tm => tm.ExpectedPowerBins)
             .FirstAsync(t => t.TurbineId == turbineId);
--- COMP702-WindTurbine/services/DegradationAnalyser.cs
+++ COMP702-WindTurbine/services/DegradationAnalyser.cs
@@ -17,17 +17,27 @@ using Microsoft.VisualBasic;
 // try grid search
 
 public sealed class DegradationAnalyser (
-    ILogger<MonitoringWorker> logger,
-    int counter = 0
-)
+    ILogger<MonitoringWorker> logger
+    )
 {
 
     public void PerformBenchmark(ICollection<TurbineTelemetry> telemetry, Turbine turbine)
     {
-        //get SVR filepath from turbine (make sure when turbine is gotten from supabase, svr filepath is included)
+        if (turbine.DegradationModelDetails == null)
+        {
+            logger.LogWarning("Degradation Model not found for turbine {t}. Training degradation model now...", turbine.TurbineId);
+            TrainBenchmark(telemetry, turbine);
+            //Somehow re-get from supabase.
+            return;
+        }
+        var modelPath = turbine.DegradationModelDetails.Filepath;
+        var expectedDeviation = turbine.DegradationModelDetails.Offset;
 
-        //test data against the SVR model to get residuals.
-        //use averaging equation (in reserach doc) to get single deviation value
+        //get model file
+        //pass file and expectedDeviation to python: (if ONNX, this can all be done in C#)
+
+        //test data against the SVR model to get residuals. 
+        //use averaging equation (in reserach doc) to get deviation value
         //subtract it from expected deviation
 
         //save results to supabase
@@ -47,9 +57,41 @@ public sealed class DegradationAnalyser (
         var (region2, region2point5) = SplitRegion(telemetry, turbine.TurbineModel);
 
         //TrainModel(region2, true);
-        TrainModel(region2point5, false);
+        CreateModel(region2point5, false);
+        logger.LogInformation("Degradation Benchmark Models successfuly created for turbine {t}", turbine.TurbineId);
 
     }
+
+   
+
+    private void CreateModel(ICollection<TurbineTelemetry> dataset, bool isRegion2)
+    {
+        //do a train-test split
+
+        //pass training data to python
+        //this is what python does with it:
+
+            //normalize training data?
+
+            //do grid search for best hyperprams using training data
+                //either have them be tightly bunched assumed-correct params,
+                //OR, do a double grid search where you take the best params and do a tighter search around that. 
+
+
+            //validation, not for production: also have the testing data passed and do this:
+                //also test data to get RMSE, R2, MAE values
+                //put test/actual data into grapher
+
+
+        //python returns serialized model
+            //serialized using ONNX?
+
+        //use test data to get residuals; use averaging equation to get expected-deviation
+
+        //save SVR to file
+        //upload SVR filename and expected deviation to supabase
+    }
+
 
     static private (ICollection<TurbineTelemetry>, ICollection<TurbineTelemetry>) SplitRegion(ICollection<TurbineTelemetry> telemetry, TurbineModel turbineModel)
     {
@@ -68,175 +110,5 @@ public sealed class DegradationAnalyser (
         }
 
         return (region2, region2point5);
-    }
-
-    private void TrainModel(ICollection<TurbineTelemetry> dataset, bool isRegion2)
-    {
-        // ###### TODO
-        // ###### TODO When having model training happen, make sure it awaits/threads properly so it doesn't hold up the whole windows service worker loop
-        // ###### TODO
-
-        double[] inputRaw = [];
-        double[] outputRaw = dataset.Select(d => d.PowerOutput).ToArray();
-        if (isRegion2)
-        {
-            inputRaw = dataset.Select(d => d.GeneratorSpeed).ToArray();
-        } else
-        {
-            inputRaw = dataset.Select(d => d.PitchAngle).ToArray();
-        }
-
-
-        bool doNormalize = false;
-        double[][] input = [];
-        double[] output = [];
-
-        if (doNormalize)
-        {   
-            //normalize input and outputs to 0-1
-            double inputMin = inputRaw.Min();
-            double inputNormalizationFactor = inputRaw.Max() - inputMin;
-            double[] normInput = inputRaw.Select(d => (d-inputMin)/inputNormalizationFactor).ToArray();
-
-            double outputMin = outputRaw.Min();
-            double outputNormalizationFactor = outputRaw.Max() - outputMin;
-            output = outputRaw.Select(d => (d-outputMin)/outputNormalizationFactor).ToArray();
-
-            input = normInput.Select(value => new double[] { value }).ToArray();
-        } else
-        {
-            input = inputRaw.Select(value => new double[] { value }).ToArray();
-            output = outputRaw;
-        }
-        
-
-
-
-        //TODO testtrainsplit this randomly instead of fixed
-        int testLength = input.Length / 3;
-        int trainLength = input.Length - testLength;
-        var (trainInput, trainOutput, testInput, testOutput) = TrainTestSplit(input, output);
-
-
-
-
-
-
-        //var svrTrained = DoGridSearch(trainInput, trainOutput);
-
-
-
-
-
-
-        
-
-        var svr = new FanChenLinSupportVectorRegression<Gaussian>();
-            //according to paper, C and epsilon values are iqr(Y)/13.49
-            svr.UseComplexityHeuristic = true;
-            //svr.Complexity = 1.0; // what C and other values? omit to make it guess
-            svr.UseKernelEstimation = true;
-            svr.Kernel = new Gaussian(); //what gamma value?
-            svr.Epsilon = 0.01; //default 0.001
-            svr.Tolerance = 0.1; //default 0.01
-
-        
-        //training happens here
-        logger.LogInformation("now training model...");
-        SupportVectorMachine<Gaussian> svrTrained = svr.Learn(trainInput, trainOutput);
-        logger.LogInformation("Making predictions...");
-        double[] scores = svrTrained.Score(testInput);
-        
-        var topTen = scores.Take(10);
-        var topTenTest = testOutput.Take(10);
-        logger.LogInformation("SVR RESULTS HERE: {topTen}", topTen);
-        logger.LogInformation("ACTUAL VALUES HERE: {topTen}", topTenTest);
-
-
-        double[] unlistedTestInput = testInput.Select(x => x[0]).ToArray();
-        SaveAsCsv(unlistedTestInput, testOutput, scores);
-
-        //test SVR on test data to get residuals
-        //use averaging equation to get expected deviation
-
-        //serialize and save trained SVR to file
-
-        //upload SVR filepath and expected deviation to supabase
-    }
-
-    private (double[][], double[], double[][], double[]) TrainTestSplit(double[][] input, double[] output)
-    {
-        int trainLength = (int)(input.Length * 0.66);
-        double[][] trainInput = input.Take(trainLength).ToArray();
-        double[] trainOutput = output.Take(trainLength).ToArray();
-        double[][] testInput = input.Skip(trainLength).ToArray();
-        double[] testOutput = output.Skip(trainLength).ToArray();
-
-        return (trainInput, trainOutput, testInput, testOutput);
-    }
-
-
-
-private void SaveAsCsv(double[] input, double[] output, double[] scores)
-    {
-        var lines = new List<string>{$"GeneratorSpeed or PitchAngle,Power,PredictedPower"};
-        for (int i = 0; i < input.Length; i++)
-        {
-            lines.Add($"{input[i]},{output[i]},{scores[i]}");
-        }
-
-        File.WriteAllLines($"TestCSVs/Transform-r2.5.csv", lines);
-        counter++;
-    }
-
-private void TEMPSaveAsCsv(double[][] input, double[] output, string name)
-    {
-        double[] normalInput = input.Select(x => x[0]).ToArray();
-
-        var lines = new List<string>{$"PitchAngle,Power"};
-        for (int i = 0; i < input.Length; i++)
-        {
-            lines.Add($"{normalInput[i]},{output[i]}");
-        }
-
-        File.WriteAllLines($"TestCSVs/{name}.csv", lines);
-        counter++;
-    }
-
-private SupportVectorMachine<Gaussian> DoGridSearch(double[][] input, double[] output)
-    {
-        logger.LogInformation("Starting grid search...");
-        var gridsearch = GridSearch<double[], double>.Create(
-            ranges: new
-            {
-                Complexity = GridSearch.Values( 0.01, 0.1, 1, 10, 100 ),
-                Gamma = GridSearch.Values(0.001, 0.01, 0.1, 0.5, 1, 10 )
-            },
-
-            learner: (p) => new FanChenLinSupportVectorRegression<Gaussian>
-            {
-                Complexity = p.Complexity,
-                Kernel = Gaussian.FromGamma(p.Gamma),
-                Epsilon = 0.001, //default 0.001
-                Tolerance = 0.01, //default 0.01
-                
-            },
-
-            fit: (teacher, x, y, w) => teacher.Learn(x, y, w),
-
-            loss: (actual, expected, m) => new SquareLoss(expected).Loss(actual)            
-            
-
-        );
-
-        gridsearch.ParallelOptions.MaxDegreeOfParallelism = 4;
-
-        var result = gridsearch.Learn(input, output);
-        logger.LogInformation("Grid search Finished");
-
-        var bestSvm = result.BestModel;
-        logger.LogInformation("Best parameters - Compexity:{c} Gamma:{g}", result.BestParameters.Complexity.Value, result.BestParameters.Gamma.Value);
-        //bestSvm.Save("./");
-        return bestSvm;
     }
 }
