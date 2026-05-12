d14aae1d60ea9c584336593ab3494a7800070da0
PALPAPIG <50583794+palpapig@users.noreply.github.com>
2026-05-12 16:14:49 +1200

    Handle empty benchmark telemetry

--- Worker/services/Benchmarker.cs
+++ Worker/services/Benchmarker.cs
@@ -1,4 +1,5 @@
 namespace COMP702_WindTurbine.services;
+
 using COMP702_WindTurbine.models;
 public sealed class PowerBin
 {
@@ -7,7 +8,7 @@ public sealed class PowerBin
 }
 
 
-public sealed class Benchmarker (
+public sealed class Benchmarker(
     ILogger<MonitoringWorker> logger,
     IServiceScopeFactory scopeFactory)
 
@@ -17,61 +18,62 @@ public sealed class Benchmarker (
     {
         DateTime endDate = benchmarkEndDate ?? DateTime.Now;
         using (var tempScope = scopeFactory.CreateScope())
-            {
-                var tempDbService = tempScope.ServiceProvider.GetRequiredService<DbService>();
+        {
+            var tempDbService = tempScope.ServiceProvider.GetRequiredService<DbService>();
 
-                Turbine turbine = await tempDbService.GetTurbineById(turbineId);
+            Turbine turbine = await tempDbService.GetTurbineById(turbineId);
 
-                //TODO get data from csv
-                List<TurbineTelemetry> yearTelemetry = await tempDbService.GetTurbineDataYear(turbineId, endDate.Year);
-                logger.LogInformation("turbine name: {tname}", turbine.Name);
-                    
-                BenchmarkResult benchmarkResult = Benchmark(yearTelemetry, turbine);
-                await tempDbService.AddBenchmarkResultAsync(benchmarkResult);
-                logger.LogInformation("Successfully written benchmark results to database");
+            //TODO get data from csv
+            List<TurbineTelemetry> yearTelemetry = await tempDbService.GetTurbineDataYear(turbineId, endDate.Year);
+            logger.LogInformation("turbine name: {tname}", turbine.Name);
 
-            }
+            BenchmarkResult benchmarkResult = Benchmark(yearTelemetry, turbine);
+            await tempDbService.AddBenchmarkResultAsync(benchmarkResult);
+            logger.LogInformation("Successfully written benchmark results to database");
+
+        }
     }
 
-    public async Task DoAnalysisIfNeeded(Turbine turbine, int monthsGap = 3){
+    public async Task DoAnalysisIfNeeded(Turbine turbine, int monthsGap = 3)
+    {
         using (var tempScope = scopeFactory.CreateScope())
+        {
+            var tempDbService = tempScope.ServiceProvider.GetRequiredService<DbService>();
+
+            //if there is at least one benchmark result, AND the most recent one happened less than monthGap months ago, don't do analysis
+            if (turbine.BenchmarkResults.Count > 0)
             {
-                var tempDbService = tempScope.ServiceProvider.GetRequiredService<DbService>();
-                
-                //if there is at least one benchmark result, AND the most recent one happened less than monthGap months ago, don't do analysis
-                if (turbine.BenchmarkResults.Count > 0)
+                DateTime lastAnalysed = turbine.BenchmarkResults.MaxBy(r => r.TimeRangeEnd).TimeRangeEnd;
+                if (lastAnalysed.AddMonths(monthsGap) > DateTime.Now)
                 {
-                    DateTime lastAnalysed = turbine.BenchmarkResults.MaxBy(r => r.TimeRangeEnd).TimeRangeEnd;
-                    if (lastAnalysed.AddMonths(monthsGap) > DateTime.Now)
-                    {
-                        logger.LogInformation("No benchmarking needed, latest benchmark for turbine {t} happened less than {m} months ago", turbine.TurbineId, monthsGap);
-                        return;
-                    }
-                }
-                
-
-                //TODO get data from csv
-                List<TurbineTelemetry> benchmarkTelemetry = await tempDbService.GetTurbineDataYear("BK-TEST-4", DateTime.Now.Year);
-
-                if (benchmarkTelemetry.Count == 0)
-                {
-                    logger.LogWarning("Attempted to do degradation analysis on turbine {t}, but no data exists for given time period", turbine.TurbineId);
+                    logger.LogInformation("No benchmarking needed, latest benchmark for turbine {t} happened less than {m} months ago", turbine.TurbineId, monthsGap);
                     return;
                 }
-                
-                BenchmarkResult benchmarkResult = Benchmark(benchmarkTelemetry, turbine);
-                if (benchmarkResult != null)
-                {
-                    await tempDbService.AddBenchmarkResultAsync(benchmarkResult);
-                    logger.LogInformation("Successfully written benchmark results to database");
-                }
-
             }
+
+
+            //TODO get data from csv
+            List<TurbineTelemetry> benchmarkTelemetry = await tempDbService.GetTurbineDataYear("BK-TEST-4", DateTime.Now.Year);
+
+            if (benchmarkTelemetry.Count == 0)
+            {
+                logger.LogWarning("Attempted to do degradation analysis on turbine {t}, but no data exists for given time period", turbine.TurbineId);
+                return;
+            }
+
+            BenchmarkResult benchmarkResult = Benchmark(benchmarkTelemetry, turbine);
+            if (benchmarkResult != null)
+            {
+                await tempDbService.AddBenchmarkResultAsync(benchmarkResult);
+                logger.LogInformation("Successfully written benchmark results to database");
+            }
+
+        }
     }
     public TurbineTelemetry DummyBenchmark(TurbineTelemetry telemetry)
     {
         var rand = new Random();
-        telemetry.Efficiency = Math.Round(rand.NextDouble()*100, 2);
+        telemetry.Efficiency = Math.Round(rand.NextDouble() * 100, 2);
         return telemetry;
     }
 
@@ -101,7 +103,7 @@ public sealed class Benchmarker (
 
         //For ease of calculation, all power bins are stored as dictionaries with key:value as windspeed:power
         //frequencyBins has key:value as windspeed:frequency
-        var (measuredPowerBins, frequencyBins) = BinTelemetry(telemetry, turbineModel.CutInWindSpeed, turbineModel.CutOutWindSpeed); 
+        var (measuredPowerBins, frequencyBins) = BinTelemetry(telemetry, turbineModel.CutInWindSpeed, turbineModel.CutOutWindSpeed);
 
         Dictionary<float, float> expectedPowerBins = turbineModel.ExpectedPowerBins.ToDictionary(row => row.WindSpeed, row => row.Power);
         ICollection<PowerBinDeviation> deviationPowerBins = [];
@@ -110,11 +112,11 @@ public sealed class Benchmarker (
         //each bin, get the ratio between measured power and expected power.
         //Scale that deviation ratio by the bin's frequency and add it to a running total.
         foreach (float windSpeed in measuredPowerBins.Keys)
-        {   
+        {
             float expectedPower = expectedPowerBins[windSpeed];
-            float measuredPower = measuredPowerBins[windSpeed]; 
-            float deviationRatio =  measuredPower / expectedPower;
-            float deviationDifference =  measuredPower - expectedPower;
+            float measuredPower = measuredPowerBins[windSpeed];
+            float deviationRatio = measuredPower / expectedPower;
+            float deviationDifference = measuredPower - expectedPower;
 
             deviationPowerBins.Add(new PowerBinDeviation
             {
@@ -132,7 +134,7 @@ public sealed class Benchmarker (
 
         //convert dictionary to PowerBin list
         ICollection<PowerBinMeasured> convertedMeasuredPowerBins = [.. measuredPowerBins.Select(pair => new PowerBinMeasured { WindSpeed = pair.Key, Power = pair.Value, BenchmarkResult = result })];
-        
+
         //fill in benchmark results
         result.DeviationBins = deviationPowerBins;
         result.PowerBins = convertedMeasuredPowerBins;
@@ -146,7 +148,7 @@ public sealed class Benchmarker (
     {
         //remove rows with negative minimum power output.
         telemetry = [.. telemetry.Where(row => row.MinimumPowerOutput > 0)];
-    
+
         //TODO binning or SVR to filter by blade pitch angle
 
         if (!hasCorrectedWindSpeed)
@@ -163,15 +165,15 @@ public sealed class Benchmarker (
 
         Dictionary<float, float> binnedTelemetry = [];
         Dictionary<float, int> binnedFrequency = [];
-        
+
         //for each bin...
         for (float i = minBin; i <= maxBin; i += binInterval)
-        {   
-            float lowerBound = i - (binInterval/2);
-            float upperBound = i + (binInterval/2);
-            
+        {
+            float lowerBound = i - (binInterval / 2);
+            float upperBound = i + (binInterval / 2);
+
             //filter for rows in bin (where windspeed is in range)
-            ICollection<TurbineTelemetry> binRows = [.. telemetry.Where(row => lowerBound <= row.CorrectedWindSpeed && row.CorrectedWindSpeed < upperBound )];
+            ICollection<TurbineTelemetry> binRows = [.. telemetry.Where(row => lowerBound <= row.CorrectedWindSpeed && row.CorrectedWindSpeed < upperBound)];
 
             if (binRows.Count == 0)
             {
@@ -179,7 +181,7 @@ public sealed class Benchmarker (
             }
 
             //store average power and # of rows for that windspeed
-            float binnedPower = (float) binRows.Average(row => row.PowerOutput);
+            float binnedPower = (float)binRows.Average(row => row.PowerOutput);
             binnedTelemetry[i] = binnedPower;
             binnedFrequency[i] = binRows.Count;
         }
